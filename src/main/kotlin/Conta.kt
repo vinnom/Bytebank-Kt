eda6143ca006f253574c62dc7766f96c0b42a73e
vinnom <vinnom.2d@gmail.com>
2020-09-27 01:14:17 -0300

    criação da classe Conta para testes e aprendizado de OO em Kotlin

---
+++ src/main/kotlin/Conta.kt
@@ -0,0 +1,28 @@
+class Conta(var titular: String, val numero: Int) {
+   var saldo: Double = 0.0
+      private set
+
+   fun deposita(valor: Double): Boolean {
+      if(valor > 0) {
+         this.saldo += valor
+         return true
+      }
+      return false
+   }
+
+   fun saca(valor: Double): Boolean {
+      if(valor > 0 && valor <= this.saldo) {
+         this.saldo -= valor
+         return true
+      }
+      return false
+   }
+
+   fun transfere(valor: Double, conta: Conta): Boolean {
+      if(this.saca(valor)) {
+         conta.deposita(valor)
+         return true
+      }
+      return false
+   }
+}
